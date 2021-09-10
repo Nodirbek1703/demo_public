884561e546da6de354b2e1c90bbafdb2347a1a19
dotsent17 <noligarx@gmail.com>
2021-09-10 15:58:10 +0500

    1  ADDED

--- src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
@@ -1,15 +1,35 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Invoice;
+import com.example.demo.repository.InvoiceRepository;
 import com.example.demo.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
+    final
+    InvoiceRepository invoiceRepository;
+
+    public InvoiceServiceImpl(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
+
+
     @Override
     public List<Invoice> expiredInvoices() {
+        return invoiceRepository.findAllByIssuedDateAfterDueDate();
+    }
+
+    @Override
+    public List<Invoice> wrongDateInvoices() {
+        return null;
+    }
+
+    @Override
+    public List<Invoice> overPaidInvoices() {
         return null;
     }
 }
